a4ba9edda339fa8c2dc6d429ffb7752c85a8196b
Marvan4k <romanmarvanov@gmail.com>
2025-03-26 01:58:10 +0300

    create react app and include tilewindcss

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App">
+      <p className="text-8xl text-red-700 text-center">Привет, Виталя! Мы тут сайт делаем!</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Укажите пути к вашим файлам
+    "./public/index.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
